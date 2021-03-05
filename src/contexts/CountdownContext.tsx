a378d20a70b801bcdf498a1ec11da49418b3434e
IanaCris <ianasousati@gmail.com>
2021-03-05 19:45:20 -0300

    25 minutes update

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -37,7 +37,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     clearTimeout(countdownTimeout);
     setIsActive(false);
     setHasFinished(false);
-    setTime(0.1 * 60);//Volta a ter 25min
+    setTime(25 * 60);//Volta a ter 25min
   }
 
   useEffect(() => {
